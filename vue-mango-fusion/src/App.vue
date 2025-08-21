6c326d7ac01d07514534445176b8b1affccc3ef0
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-21 10:47:39 +0500

    Header and Footer rendered in App.vue.

--- vue-mango-fusion/src/App.vue
+++ vue-mango-fusion/src/App.vue
@@ -1,7 +1,12 @@
 <style scoped></style>
 
 <template>
+  <Header></Header>
   <router-view></router-view>
+  <Footer></Footer>
 </template>
 
-<script setup></script>
+<script setup>
+  import Header from './components/layout/Header.vue';
+  import Footer from './components/layout/Footer.vue';
+</script>
