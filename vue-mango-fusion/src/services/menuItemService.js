9a055d857707bffdcc07a8bc12abea66dc25176a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-22 03:34:47 +0500

    CreateMenuItem implemented.

--- vue-mango-fusion/src/services/menuItemService.js
+++ vue-mango-fusion/src/services/menuItemService.js
@@ -15,5 +15,20 @@ export default {
             console.error(error);
             throw error;
         }
+    },
+    async CreateMenuItem(formData){
+        try{
+            const response = await api.post('/api/menuitem', formData);
+            console.log(response);
+            if(response.data.isSuccess){
+                return response.data.data;
+            }
+            else{
+                throw new Error('Unable to create menuitem');
+            }
+        }catch(error){
+            console.error(error);
+            throw error;
+        }
     }
 }
