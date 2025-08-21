3d2ec4a3202614abe74ab601f5519835301cc09c
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-22 02:51:37 +0500

    MenuItemUpsert design enhanced with dynamic binding.

--- vue-mango-fusion/src/views/menu-item/MenuItemUpsert.vue
+++ vue-mango-fusion/src/views/menu-item/MenuItemUpsert.vue
@@ -1,19 +1,20 @@
 <template>
-    <div class="d-flex justify-content-center align-items-center">
-    <div class="spinner-grow text-success" style="width: 2.5rem; height: 2.5rem" role="status">
-      <span class="visually-hidden">Loading...</span>
+    <div class="d-flex justify-content-center align-items-center" v-if="isLoading">
+        <div class="spinner-grow text-success" style="width: 2.5rem; height: 2.5rem" role="status">
+        <span class="visually-hidden">Loading...</span>
+        </div>
     </div>
-  </div>
 
-  <div class="container">
+  <div class="container" v-else>
     <div class="mx-auto">
       <div class="mb-4 border-bottom d-flex justify-content-between align-items-center py-3">
         <h3 class="fw-semibold text-success">Add Menu</h3>
         <div class="d-flex gap-3">
           <button
-            type="submit"
+            type="button"
             form="menuForm"
             class="btn btn-success btn-sm gap-2 rounded-1 px-4 py-2"
+            @click="console.log(menuItem);"
           >
             <span class="spinner-border spinner-border-sm me-2"></span>
             Create Item
@@ -24,10 +25,10 @@
           </button>
         </div>
       </div>
-      <div class="alert alert-danger pb-0">
+      <div class="alert alert-danger pb-0" v-if="errorList.length">
         Please fix the following errors:
         <ul>
-          <li>ERROR</li>
+          <li v-for="error in errorList" :key="error">{{ error }}</li>
         </ul>
       </div>
       <form enctype="multipart/form-data" class="needs-validation" id="menuForm">
@@ -36,13 +37,14 @@
             <div class="d-flex flex-column g-12">
               <div class="mb-3">
                 <label for="name" class="form-label">Item Name</label>
-                <input id="name" type="text" class="form-control" placeholder="Enter item name" />
+                <input id="name" v-model="menuItem.name" type="text" class="form-control" placeholder="Enter item name" />
               </div>
 
               <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
                 <textarea
                   id="description"
+                  v-model="menuItem.description"
                   class="form-control"
                   placeholder="Describe the menu item..."
                   rows="3"
@@ -53,6 +55,7 @@
                 <label for="specialTag" class="form-label">Special Tag (Optional)</label>
                 <input
                   id="specialTag"
+                  v-model="menuItem.specialTag"
                   type="text"
                   class="form-control"
                   placeholder="e.g., Chef's Special"
@@ -61,14 +64,15 @@
 
               <div class="mb-3">
                 <label for="category" class="form-label">Category</label>
-                <select id="category" class="form-select">
-                  <option>CATEGORY</option>
+                <select id="category" class="form-select" v-model="menuItem.category">
+                  <option selected disabled>--Select a Category--</option>
+                  <option v-for="category in CATEGORIES" :key="category">{{ category }}</option>
                 </select>
               </div>
 
               <div class="mb-3">
                 <label for="price" class="form-label">Price</label>
-                <input id="price" class="form-control" />
+                <input id="price" class="form-control" v-model.number="menuItem.price" />
               </div>
             </div>
           </div>
@@ -92,3 +96,21 @@
     </div>
   </div>
 </template>
+
+<script setup>
+    import { ref, reactive, onMounted } from 'vue';
+    import { useRoute, useRouter } from 'vue-router';
+    import { API_URL } from '@/constants/config';
+    import { CATEGORIES } from '@/constants/constants';
+
+    const isLoading = ref(false);
+    const menuItem = reactive({
+        name: '',
+        description: '',
+        category: '',
+        specialTag: '',
+        price: 0.00,
+        imageURL: ''
+    });
+    const errorList = reactive([]);
+</script>
