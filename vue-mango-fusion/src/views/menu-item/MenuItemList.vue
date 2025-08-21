226029e59b41cb34923e7b0f6e09d5b8cd644eca
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-22 02:14:36 +0500

    MenuItemList design enahnced with dynamic binding.

--- vue-mango-fusion/src/views/menu-item/MenuItemList.vue
+++ vue-mango-fusion/src/views/menu-item/MenuItemList.vue
@@ -7,14 +7,12 @@
         </div>
 
         <div v-else>
-            <div
-            class="card-header d-flex flex-column flex-md-row justify-content-between align-items-md-center p-3"
-            >
+            <div class="card-header d-flex flex-column flex-md-row justify-content-between align-items-md-center p-3">
             <div>
                 <h2 class="h5 text-success">Menu Items</h2>
                 <p class="mb-0 text-muted small">Manage your restaurant's offerings</p>
             </div>
-            <button class="btn btn-success btn-sm gap-2 rounded-1 px-4 py-2">
+            <button class="btn btn-success btn-sm gap-2 rounded-1 px-4 py-2" @click="router.push({name: APP_ROUTE_NAMES.CREATE_MENU_ITEM})">
                 <i class="bi bi-plus-square"></i> &nbsp;
                 <span>Add Item</span>
             </button>
@@ -22,51 +20,51 @@
             <div class="card-body p-3">
             <div class="table-responsive">
                 <table class="table table-hover align-middle mb-0">
-                <thead>
-                    <tr>
-                    <th class="ps-3 small text-muted">Item</th>
-                    <th class="small text-muted">Category</th>
-                    <th class="small text-muted">Price</th>
-                    <th class="small text-muted">Tag</th>
-                    <th class="pe-3 text-end small text-muted">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td class="ps-3">
-                        <div class="d-flex align-items-center">
-                        <img
-                            src="https://placehold.co/600x400"
-                            alt="Item"
-                            class="rounded object-fit-cover me-2"
-                            style="width: 50px; height: 50px"
-                        />
-                        <div>
-                            <div class="fw-semibold small">NAME</div>
-                        </div>
-                        </div>
-                    </td>
-                    <td>
-                        <span class="badge bg-success bg-opacity-10 text-success small">
-                        CATEGORY
-                        </span>
-                    </td>
-                    <td class="fw-semibold small">$$$</td>
-                    <td>
-                        <span class="badge bg-info bg-opacity-10 text-info small"> TAG </span>
-                    </td>
-                    <td class="pe-3 text-end">
-                        <div class="d-flex gap-2 justify-content-end">
-                        <button class="btn btn-sm btn-outline-success">
-                            <i class="bi bi-pencil-square"></i>
-                        </button>
-                        <button class="btn btn-sm btn-outline-danger">
-                            <i class="bi bi-trash3-fill"></i>
-                        </button>
-                        </div>
-                    </td>
-                    </tr>
-                </tbody>
+                    <thead>
+                        <tr>
+                        <th class="ps-3 small text-muted">Item</th>
+                        <th class="small text-muted">Category</th>
+                        <th class="small text-muted">Price</th>
+                        <th class="small text-muted">Tag</th>
+                        <th class="pe-3 text-end small text-muted">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="menuItem in menuItems" :key="menuItem.id">
+                            <td class="ps-3">
+                                <div class="d-flex align-items-center">
+                                <img
+                                    :src="`${API_URL}/${menuItem.imageURL}`"
+                                    alt="Item"
+                                    class="rounded object-fit-cover me-2"
+                                    style="width: 50px; height: 50px"
+                                />
+                                <div>
+                                    <div class="fw-semibold small">{{ menuItem.Name }}</div>
+                                </div>
+                                </div>
+                            </td>
+                            <td>
+                                <span class="badge bg-success bg-opacity-10 text-success small">
+                                {{ menuItem.category }}
+                                </span>
+                            </td>
+                            <td class="fw-semibold small">$ {{ menuItem.price }}</td>
+                            <td>
+                                <span class="badge bg-info bg-opacity-10 text-info small"> {{ menuItem.specialTag }} </span>
+                            </td>
+                            <td class="pe-3 text-end">
+                                <div class="d-flex gap-2 justify-content-end">
+                                <button class="btn btn-sm btn-outline-success" @click="router.push({name: `${APP_ROUTE_NAMES.EDIT_MENU_ITEM}`, params: {id: `${menuItem.id}`}})">
+                                    <i class="bi bi-pencil-square"></i>
+                                </button>
+                                <button class="btn btn-sm btn-outline-danger">
+                                    <i class="bi bi-trash3-fill"></i>
+                                </button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
             </div>
@@ -75,11 +73,15 @@
 </template>
 
 <script setup>
-    import menuItemService from '@/services/menuItemService';
+    import { APP_ROUTE_NAMES } from '@/constants/routeNames';
     import {ref, onMounted, reactive} from 'vue';
+    import { useRouter } from 'vue-router';
+    import menuItemService from '@/services/menuItemService';
+import { API_URL } from '@/constants/config';
 
     const menuItems = reactive([]);
     const isLoading = ref(false);
+    const router = useRouter();
 
     async function FetchMenuItems(){
         try {
