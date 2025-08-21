4da22aee153907bf9d630c9fc5f585320872bddd
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-21 10:58:33 +0500

    App and footer components design enhanced.

--- vue-mango-fusion/src/App.vue
+++ vue-mango-fusion/src/App.vue
@@ -1,9 +1,13 @@
 <style scoped></style>
 
 <template>
-  <Header></Header>
-  <router-view></router-view>
-  <Footer></Footer>
+  <div class="d-flex flex-column py-2 min-vh-100 container-md">
+    <Header></Header>
+    <main class="flex-grow-1 mx-lg-auto w-100">
+      <router-view></router-view>
+    </main>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script setup>
--- vue-mango-fusion/src/components/layout/Footer.vue
+++ vue-mango-fusion/src/components/layout/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="footer text-center mx-auto mt-4 p-3 bg-success rounded-3">
-        &copy; Made with Love By Tayyab Arsalan.
+    <div class="footer text-center mx-auto mt-4 p-3 bg-opacity-10 text-success bg-success rounded-3">
+        &copy; Made with <i class="bi bi-heart-fill"></i> By Tayyab Arsalan.
     </div>
 </template>
