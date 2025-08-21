1cd4f7e4c101be5ddb1a451f23ecda3f83c98b18
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-21 10:51:23 +0500

    Bootstrap and bootstrap icons imported and included.

--- vue-mango-fusion/src/main.js
+++ vue-mango-fusion/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './App.vue'
 import router from './router/routes'
 
