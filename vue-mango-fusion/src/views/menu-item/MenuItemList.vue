389489f54c8773de74534de28a860284b43c8699
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-22 01:32:37 +0500

    FetchMenuItems implemented in MenuItem list component.

--- vue-mango-fusion/src/views/menu-item/MenuItemList.vue
+++ vue-mango-fusion/src/views/menu-item/MenuItemList.vue
@@ -1,3 +1,29 @@
 <template>
     Menu Item List
 </template>
+
+<script setup>
+    import menuItemService from '@/services/menuItemService';
+    import {ref, onMounted} from 'vue';
+
+    const menuItems = ref([]);
+    const isLoading = ref(false);
+
+    async function FetchMenuItems(){
+        try {
+            isLoading.value = true;
+            menuItems.value = await menuItemService.GetMenuItems();
+            console.log(menuItems.value);
+        } catch (error) {
+            console.error(error);
+        }
+        finally{
+            isLoading.value = false;
+        }
+    }
+
+    onMounted(()=>{
+        FetchMenuItems();
+    });
+
+</script>
