70234f7033fbc060a006a112176d4f5602309e48
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-24 15:36:46 +0500

    Constants added for Roles.

--- vue-mango-fusion/src/constants/constants.js
+++ vue-mango-fusion/src/constants/constants.js
@@ -6,3 +6,8 @@ export const SORT_PRICE_LOW_HIGH = 'PRICE LOW-HIGH';
 export const SORT_PRICE_HIGH_LOW = 'PRICE HIGH-LOW';
 
 export const SORT_OPTIONS = [SORT_NAME_A_Z, SORT_NAME_Z_A, SORT_PRICE_LOW_HIGH, SORT_PRICE_HIGH_LOW];
+
+export const ROLE_ADMIN = 'Admin';
+export const ROLE_CUSTOMER = 'Customer';
+
+export const ROLES = [ROLE_ADMIN, ROLE_CUSTOMER];
