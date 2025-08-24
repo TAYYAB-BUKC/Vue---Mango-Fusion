b902117b2d06771cc8add919b6a5d68a9f6ce5e2
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-24 23:28:07 +0500

    OrderListCard properties bindings implemented using props.

--- vue-mango-fusion/src/components/card/OrderListCard.vue
+++ vue-mango-fusion/src/components/card/OrderListCard.vue
@@ -4,9 +4,9 @@
       <div class="d-flex justify-content-between align-items-center">
         <div class="d-flex align-items-center">
           <i class="bi bi-receipt-cutoff text-success pe-2" style="font-size: 2rem"></i>
-          <h5 class="card-title mb-0">Order #</h5>
+          <h5 class="card-title mb-0">Order # {{ order?.id }}</h5>
         </div>
-        <span class="bg-danger-subtle text-danger-emphasis"> STATUS </span>
+        <span class="bg-danger-subtle text-danger-emphasis"> {{ order?.status }} </span>
       </div>
     </div>
     <div class="card-body">
@@ -14,20 +14,20 @@
         <div class="d-flex align-items-center mb-3">
           <i class="bi bi-person-square text-success pe-2" style="font-size: 1.5rem"></i>
           <div>
-            <h6 class="mb-0">NAME</h6>
-            <small class="text-body-secondary">PHONE</small>
+            <h6 class="mb-0">{{ order?.name }}</h6>
+            <small class="text-body-secondary">{{ order?.phoneNumber }}</small>
           </div>
         </div>
         <div class="d-flex align-items-center mb-2">
           <i class="bi bi-envelope text-success pe-2"></i>
-          <span>EMAIL</span>
+          <span>{{ order?.email }}</span>
         </div>
         <div class="d-flex align-items-center mb-2">
           <i class="bi bi-bag text-success pe-2"></i>
-          <span>XX items</span>
+          <span>{{ order?.orderItems }} items</span>
         </div>
         <div class="d-flex align-items-center">
-          <i class="bi bi-cash-coin text-success pe-2"></i><span class="fw-bold">$$$</span>
+          <i class="bi bi-cash-coin text-success pe-2"></i><span class="fw-bold">$ {{ order?.orderTotal.toFixed(2) }}</span>
         </div>
       </div>
 
@@ -36,14 +36,14 @@
           <i class="bi bi-card-list text-success pe-2"></i>
           <h6 class="mb-0">Order Items</h6>
         </div>
-        <div class="mb-3 pb-3 border-bottom">
+        <div class="mb-3 pb-3 border-bottom" v-for="orderDetails in order.details" :key="orderDetails.id">
           <div class="d-flex justify-content-between align-items-center mb-2">
             <div class="d-flex align-items-center">
-              <span class="text-truncate me-3">NAME</span>
+              <span class="text-truncate me-3">{{ orderDetails?.itemName }}</span>
             </div>
             <div class="d-flex align-items-center">
-              <span class="badge bg-success-subtle text-success me-2">QTY x</span>
-              <span class="text-body-secondary">$$$</span>
+              <span class="badge bg-success-subtle text-success me-2">{{ orderDetails?.quantity }} x</span>
+              <span class="text-body-secondary">$ {{ orderDetails?.price.toFixed(2) }}</span>
             </div>
           </div>
         </div>
@@ -51,3 +51,9 @@
     </div>
   </div>
 </template>
+
+<script setup>
+    const props = defineProps({
+        order: Object
+    });
+</script>
--- vue-mango-fusion/src/views/order/OrderList.vue
+++ vue-mango-fusion/src/views/order/OrderList.vue
@@ -26,7 +26,7 @@
     <div class="row g-4" v-if="orders.length > 0">
       <div class="col-md-6 col-lg-4" v-for="order in orders" :key="order.id">
         <!-- Order Card -->
-        <OrderListCard />
+        <OrderListCard :order="order" />
       </div>
     </div>
   </div>
