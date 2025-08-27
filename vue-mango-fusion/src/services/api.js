46618fbff38d77bd2dfa68b6e586b8ab024fa751
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-08-27 23:33:31 +0500

    Securitytoken added for each request using request interceptors in api base setup.

--- vue-mango-fusion/src/services/api.js
+++ vue-mango-fusion/src/services/api.js
@@ -1,8 +1,20 @@
 import { API_URL } from "@/constants/config";
+import { TOKEN_COOKIE_NAME } from "@/constants/constants";
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export const api = axios.create({
     baseURL: API_URL
 });
+
+api.interceptors.request.use((configuration) => {
+    const securityToken = Cookies.get(TOKEN_COOKIE_NAME);
+    if(securityToken){
+        configuration.headers.Authorization = `Bearer ${securityToken}`;
+    }
+
+    return configuration;
+    }, (error) => Promise.reject(error)
+);
 
 export default api;
